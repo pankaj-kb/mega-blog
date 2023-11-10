a5b23a637bd2ad8398e966162745b3ff3089766c
Pankaj Bhadane <pankajb2601@gmail.com>
2023-11-10 16:10:56 +0530

    created authentication service

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
-import { useState } from "react";
-
 import "./App.css";
 
 function App() {
+
   return (
     <>
-   <h1>A blog app with appwrite</h1>
+      <h1>A blog app with appwrite</h1>
     </>
   );
 }
