1e40dcbdba12f98ec9bcac2e3a11d226b6ab998d
Pankaj Bhadane <pankajb2601@gmail.com>
2023-11-11 15:13:54 +0530

    added backend methods

---
+++ src/appwrite/config.js
@@ -0,0 +1,116 @@
+import { resolvePackageEntry } from "vite";
+import conf from "../conf/conf";
+
+import { Client, Databases, ID, Storage, Query } from "appwrite"
+
+export class Service {
+    client = new Client();
+    databases;
+    bucket;
+
+    constructor() {
+        this.client
+            .setEndpoint(conf.appWriteUrl)
+            .setProject(conf.setProject)
+        this.databases = new Databases(this.client)
+        this.bucket = new Storage(this.client)
+    }
+
+    async createPost({ title, slug, content, featuredImage, status, userId }) {
+        try {
+            return await this.databases.createDocument(
+                conf.appWriteDataBaseId, conf.appWriteCollectionId, slug {
+                title, content, featuredImage, status, userId
+            }
+            )
+        } catch (error) {
+            console.error(error)
+        }
+    }
+
+    async updatePost(slug, { title, content, featuredImage, status }) {
+        try {
+            return await this.databases.updateDocument(
+                conf.appWriteDataBaseId,
+                conf.appWriteCollectionId,
+                slug,
+                {
+                    title,
+                    content,
+                    featuredImage,
+                    status
+                }
+            )
+        } catch (error) {
+            console.error(error)
+        }
+    }
+
+    async deletePost(slug) {
+        try {
+            await this.databases.deleteDocument(
+                conf.appWriteDataBaseId,
+                conf.appWriteCollectionId,
+                slug
+            )
+            return true
+        } catch (error) {
+            console.error(error)
+            return false
+        }
+    }
+
+    async getPost(slug) {
+        try {
+            return await this.databases.getDocument(
+                conf.appWriteDataBaseId,
+                conf.appWriteCollectionId,
+                slug
+            )
+        } catch (error) {
+            console.error(error)
+        }
+    }
+
+    async getPosts(query = [Query.equal("status", "active")]) {
+        try {
+            return await this.databases.listDocuments(
+                conf.appWriteDataBaseId,
+                conf.appWriteCollectionId,
+                query,
+            )
+            return false
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    async uploadFile(file) {
+        try {
+            return await this.bucket.createFile(
+                conf.appWriteBucketId,
+                ID.unique(),
+                file
+            )
+        } catch (error) {
+            console.log(error)
+            return false;
+        }
+    }
+
+    async deleteFile(fileId) {
+        try {
+            await this.bucket.deleteFile(
+                conf.appWriteBucketId,
+                fileId
+            )
+        } catch (error) {
+            console.error(error)
+            return false;
+        }
+    }
+}
+
+const service = new Service()
+
+export default service
