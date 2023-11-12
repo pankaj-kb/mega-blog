a105f54d8ba2cbe016bc048b79a941a5c56dc294
Pankaj Bhadane <pankajb2601@gmail.com>
2023-11-12 14:47:13 +0530

    added redux sote

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,31 @@
+import { useState } from "react";
 import "./App.css";
+import { useDispatch } from "react-redux";
+import authService from "./appwrite/auth";
+import { useEffect } from "react";
+import { login, logout } from "./store/authSlice";
 
 function App() {
+  const [loading, setLoading] = useState(true);
 
-  return (
-    <>
-      <h1>A blog app with appwrite</h1>
-    </>
-  );
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => setLoading(false));
+  }, []);
+
+  return !loading ? () : (
+    <h1>Loading</h1>
+  )
 }
 
 export default App;
