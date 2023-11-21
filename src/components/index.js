775d7fab91530caeb1d5633ac70a1db6cc60d8e0
Pankaj Bhadane <pankajb2601@gmail.com>
2023-11-22 01:04:29 +0530

    done with pages and initial routing

--- src/components/index.js
+++ src/components/index.js
@@ -4,7 +4,26 @@ import Container from "./container/Container";
 import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
 import RTE from "./RTE";
+import Signup from "./Signup"
+import Login from "./Login"
+import PostCard from "./PostCard"
+import AuthLayout from "./AuthLayout"
+import Button from "./Button"
+import Select from "./Select"
+import PostForm from "./post-form/PostForm"
 
 export {
-    Header, Footer, Container, Logo, LogoutBtn, RTE
+    Header,
+    Footer,
+    Container,
+    Logo,
+    LogoutBtn,
+    RTE,
+    Signup,
+    Login,
+    PostCard,
+    AuthLayout,
+    Button,
+    Select,
+    PostForm
 }
